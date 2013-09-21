5351a3ece66836aeb5c453ba4d3af74a24041f32
David Duvenaud <dkd23@cam.ac.uk>
2013-09-21 15:10:22 +0100

    Coded up a synthetic example of a good latent representation

--- source/ae_rep.m
+++ source/ae_rep.m
@@ -1,5 +1,4 @@
-clear all;
-close all;
+function ae_rep
 
 addpath('utils');
 addpath('nlpca');
@@ -10,23 +9,39 @@ randn('state',seed);
 rand('state',seed);
 
 scale = 0.5;
-N = 50;
-angles = linspace(pi/6,pi + pi/2,N)';
+N = 200;
+%angles = linspace(9*pi/6,(11/6)*pi,N)';
+angles = linspace(pi/6,(11/6)*pi,N)';
+
+input_noise = 0.0005;
+output_noise = 0.001;
+
 x = (angles.*cos(angles) + randn(N,1).*0.01).* scale;
 y = (angles.*sin(angles) + randn(N,1).*0.01).* scale;
 
+x = x + randn(size(x)) .* sqrt(input_noise);
+y = y + randn(size(y)) .* sqrt(input_noise);
+
+angles = (angles - mean(angles));
+
 figure(1);
-plot(x,y, '.');
-xlims = xlim;
-ylims = ylim;
+plot(x+y,y-x, '.');
+%xlims = xlim;
+%ylims = ylim;
+
+xlims(1) = min(x) - 0.1*(max(x) - min(x));
+xlims(2) = max(x) + 0.1*(max(x) - min(x));
+ylims(1) = min(y) - 0.1*(max(y) - min(y));
+ylims(2) = max(y) + 0.1*(max(y) - min(y));
+
 
 
 data = [x,y];
 
-[c,net]=nlpca(data', 2, 'type', 'bottleneck', 'units_per_layer', [2, 15 , 2 , 15, 2]);
-figure(10); imagesc(c);
+%[c,net]=nlpca(data', 2, 'type', 'bottleneck', 'units_per_layer', [2, 15 , 2 , 15, 2]);
+%figure(10); imagesc(c);
 
-n_1d = 300;
+n_1d = 600;
 xrange = linspace( xlims(1), xlims(2), n_1d);
 yrange = linspace( ylims(1), ylims(2), n_1d);
 
@@ -34,16 +49,40 @@ yrange = linspace( ylims(1), ylims(2), n_1d);
 [xa, xb] = ndgrid( xrange, yrange ); %randn(n, D);   % Deepest latent layer.
 xgrid = [ xa(:), xb(:) ];
 
-pc = nlpca_get_components(net,xgrid');
+%pc = nlpca_get_components(net,xgrid');
 
 %pc = nlpca_get_components(net,data');
 
-colors = coord_to_color(sin(2.*pi.*pc'*10));
+sigma = se_kernel( data', data') + eye(N).*output_noise;
+k_x_xaux = se_kernel(data', xgrid');
+
+% Work out warping distribution conditional on the already sampled points.
+pc = k_x_xaux' / sigma * [angles, randn(size(angles)).*output_noise];
+
+
+colors = coord_to_color(sin(2.*pi.*pc*2));
 im = reshape(colors, n_1d, n_1d, 3);
+
+% plot the data on the image
+for i = 1:N
+    closest_x = round((x(i) - xlims(1))/(xlims(2) - xlims(1)) * n_1d);
+    closest_y = round((y(i) - ylims(1))/(ylims(2) - ylims(1)) * n_1d);
+    im(closest_x,closest_y,:) = 1;
+end
+
 figure(4);
 imshow(im);
 %set_fig_units_cm( 8,8 );
 %if savefig
-%imwrite(im, 'hidden.png', 'png' );
+imwrite(im, 'hidden.png', 'png' );
 %savepng(gcf, [basedir 'layer_0']);
 %end
+end
+
+function sigma = se_kernel(x, y)
+    if nargin == 0
+        sigma = 'Normal SE covariance.'; return;
+    end
+
+    sigma = 0.5.*exp( -0.5.*sq_dist(x, y)*5);
+end
