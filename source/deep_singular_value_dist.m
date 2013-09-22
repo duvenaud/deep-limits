92b9f3fdc22ef1ac1ad7e62b5e0cb47d3322d63c
David Duvenaud <dkd23@cam.ac.uk>
2013-09-22 19:55:49 +0100

    Updated SVD spectrum experiment

--- source/deep_singular_value_dist.m
+++ source/deep_singular_value_dist.m
@@ -4,46 +4,63 @@
 % David Duvenaud
 % August 2013
 
+function deep_singular_value_dist
+
 D = 10;    % Dimension of matrices
-L = 10;   % Number of layers
+L = 30;   % Number of layers
 n_samples = 4000;
 
 lambdas = NaN(L, n_samples, D);
+lambdas_c = NaN(L, n_samples, D);
 for i = 1:n_samples
-    J = eye(D); 
-    for l = 1:L;
-        % Generate a random matrix.
-        A = randn(D);
-        J = J*A;
+    
+    % First layer only has D inputs.
+    complete_jacob = randn(D); 
+    complete_jacob_c = complete_jacob;
+    lambdas(1, i, :) = svd( complete_jacob );  % Do SVD decomp.
+    lambdas_c(1, i, :) = svd( complete_jacob_c );  % Do SVD decomp.
+    for l = 2:L;
+        % Generate a random 2DxD matrix for this layer's Jacobian
+        new_jacob = randn(D, D);
+        new_jacob_aug = randn(D, D);
         
-        % Do SVD decomp
-        lambdas(l, i, :) = svd( J );
+        complete_jacob = new_jacob*complete_jacob;
+        
+        % Augment complete Jacobian with identity to denote that the input is
+        % being fed in.
+        new_complete_jacob_c = [complete_jacob_c, eye(D)] * [new_jacob; new_jacob_aug];
+        complete_jacob_c = complete_jacob_c + new_complete_jacob_c;
+                
+        lambdas(l, i, :) = svd( complete_jacob );  % Do SVD decomp.
+        lambdas_c(l, i, :) = svd( complete_jacob_c );  % Do SVD decomp.
     end
-    %fprintf('.');
 end
 
 % Show histograms of ratios of largest singular values to second-largest.
-
-n_bins = 100;
-
 for l = 1:L
-    
-    cur_image = NaN( n_bins, D );
-    
-    cur_lambdas = lambdas(l, :, :);
+    figure(1); clf; plot_hist(lambdas(l, :, :), D, l);
+    figure(2); clf; plot_hist(lambdas_c(l, :, :), D, l);
+    pause;
+end
+
+end
+
+function plot_hist(cur_lambdas, D, l)
+    n_bins = 100;
+
     hist_edges = linspace( 0, max(cur_lambdas(:)), n_bins);
-        
+
+    cur_image = NaN( n_bins, D );
     for d = 1:D
         % Make histogram
-        counts = histc( squeeze( lambdas(l, :, d )), hist_edges);
+        counts = histc( squeeze( cur_lambdas(1, :, d )), hist_edges);
         cur_image( :, d) = counts ./ max(counts);
     end
-    %hist(ratios(l, :), 20); 
+    %hist(ratios(l, :), 20);
     imagesc(flipdim(cur_image, 1));
     xlabel( 'singular value #')
     ylabel( 'singular value distribution');
     set( gca, 'yTick', [] );
     title(['Depth ' int2str(l)]);
-    pause;
-    %median( ratios(l, :) )
+    
 end
