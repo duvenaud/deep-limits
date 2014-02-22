b48855b1bc2ac081c4483ec33c4c2bfd400678e9
David Duvenaud <dkd23@cam.ac.uk>
2014-02-22 22:39:59 +0000

    Made singular value plots way nicer

--- source/deep_singular_value_dist.m
+++ source/deep_singular_value_dist.m
@@ -10,7 +10,7 @@ function deep_singular_value_dist
 
 D = 5;    % Dimension of matrices
 L = 50;   % Number of layers
-n_samples = 4000;
+n_samples = 100000;
 savefigs = true;
 scale = 1/3;
 seed = 0;
@@ -54,12 +54,14 @@ end
 
 % Show histograms of ratios of largest singular values to second-largest.
 for l = 1:L
-    figure(1); clf; plot_hist(lambdas(l, :, :), D, l);
+    figure(1); clf; 
+    plot_hist(squeeze(lambdas(l, :, :))./repmat(squeeze(lambdas(l, :, 1))', 1, 5), D, l);
     if savefigs
         set_fig_units_cm( 8, 6 )
         save2pdf(sprintf([basedir, 'layer-%d'], l), gcf); 
     end
-    figure(2); clf; plot_hist(lambdas_c(l, :, :), D, l);
+    figure(2); clf; %plot_hist(lambdas_c(l, :, :), D, l);
+    plot_hist(squeeze(lambdas_c(l, :, :))./repmat(squeeze(lambdas_c(l, :, 1))', 1, 5), D, l);
     if savefigs
         set_fig_units_cm( 8, 6 )
         save2pdf(sprintf([basedir, 'con-layer-%d'], l), gcf); 
@@ -76,7 +78,45 @@ end
 
 end
 
+
+
+
 function plot_hist(cur_lambdas, D, l)
+    n_bins = 50;
+
+    hist_edges = linspace( 0, max(cur_lambdas(:)), n_bins);
+
+    for d = 1:D
+        subplot(1, D, d);
+        % Make histogram
+        [counts,bins] = hist(squeeze( cur_lambdas( :, d )), hist_edges ); %# get counts and bin locations
+        barh(bins, counts, 'EdgeColor', 'none', 'BarWidth', 1)
+        
+        ylim([0, 1.1]);
+        xlim([0, max(counts) .* 1.1]);
+
+        set( gca, 'XTickLabel', '' );
+        if d > 1
+            set( gca, 'yTickLabel', '' );
+        end
+
+        sub_pos = get(gca,'position'); % get subplot axis position 
+        set(gca,'position',sub_pos.*[1 1 1.1 1]) % stretch its width and height  
+        
+        set( gca, 'XTick', [] );
+        xlabel(int2str(d))
+    end
+    %hist(ratios(l, :), 20);
+    %imagesc(flipdim(cur_image, 1));
+    %xlabel( 'Singular value #')
+    %
+    set(gcf, 'color', 'white');
+    %title(['Depth ' int2str(l)], 'Interpreter', 'Latex', 'FontSize', 18);
+    %set_fig_units_cm( 8, 6 )
+end
+
+% Plot hitogram using colors
+function plot_hist_old(cur_lambdas, D, l)
     n_bins = 100;
 
     hist_edges = linspace( 0, max(cur_lambdas(:)), n_bins);
@@ -87,14 +127,14 @@ function plot_hist(cur_lambdas, D, l)
         %hist_edges = linspace( 0, max(squeeze(cur_lambdas(:,:,d))), n_bins);
         
         % Make histogram
-        counts = histc( squeeze( cur_lambdas(1, :, d )), hist_edges);
+        counts = histc( squeeze( cur_lambdas( :, d )), hist_edges);
         cur_image( :, d) = counts ./ max(counts);
     end
     %hist(ratios(l, :), 20);
     imagesc(flipdim(cur_image, 1));
-    xlabel( 'singular value #')
-    ylabel( 'singular value distribution');
+    xlabel( 'Singular value #')
+    ylabel( 'Singular value distribution');
     set( gca, 'yTick', [] );
-    title(['Depth ' int2str(l)], 'Interpreter', 'Latex', 'FontSize', 18);
+    %title(['Depth ' int2str(l)], 'Interpreter', 'Latex', 'FontSize', 18);
 end
 
